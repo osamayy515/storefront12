26b892a6bdbb1e96ec1b669a987a5a36cfd434b7
Osama saleem <osamasaleem515@gmail.com>
2022-09-21 17:22:12 +0500

    Creating routers & overwriting the destroy method

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,20 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
+from pprint import pprint
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-detail')
-]
+router = DefaultRouter()
+router.register('products',views.ProductViewSet)
+router.register('collections',views.CollectionViewSet)
+# pprint(router.urls)
+
+urlpatterns = router.urls
+
+# urlpatterns = [
+    # path('', include(router.urls)),
+    # path('products/', views.ProductList.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetails.as_view()),
+    # path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-detail')
+# ]
--- store/views.py
+++ store/views.py
@@ -19,12 +19,10 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {'request': self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error':'product cannot be deleted because it is associated with an order item.'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
                 #Generic view
@@ -140,12 +138,10 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error':'collection cannot be deleted because it includes one or more products.'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
                 #Collection generic view
 # class CollectionList(ListCreateAPIView):
